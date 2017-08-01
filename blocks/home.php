56fe6899ed374d3232bca08f6b4939f3bb549f2f
Carlos Rubio Mac BM <carlosblondsk8@gmail.com>
2017-08-01 15:31:48 -0500

    Todos los programas

--- blocks/home.php
+++ blocks/home.php
@@ -37,7 +37,7 @@
 		<ul class="nav nav-pills nav-justified" role="tablist">
 			<li role="presentation" class="active"><a href="#cursos" aria-controls="cursos" role="tab" data-toggle="tab">Cursos</a></li>
 			<li role="presentation"><a href="#diplomado" aria-controls="diplomado" role="tab" data-toggle="tab">Diplomado</a></li>
-			<li role="presentation"><a href="#diplomado-virtual" aria-controls="diplomado-virtual" role="tab" data-toggle="tab">Diplomado Virtual</a></li>
+			<li role="presentation"><a href="#diplomado-virtual" aria-controls="diplomado-virtual" role="tab" data-toggle="tab">Programa para Profesionales</a></li>
 		</ul>
 
 		<!-- Tab panes -->
@@ -324,6 +324,8 @@
 							</div>
 						</div>
 						<!-- Termina CURSO Gestión Inmobiliaria-->
+					</div>
+					<div class="row">
 						<!-- CURSO Gerencia Financiera-->
 						<div class="col-md-6">
 							<div class="item row">
@@ -465,9 +467,357 @@
 						</div>
 						<!-- Termina CURSO Finanzas para No Financieros-->
 					</div>
+					<div class="row">
+						<!-- CURSO Administración del Riesgo y Auditoría Forense-->
+						<div class="col-md-6">
+							<div class="item row">
+								<div class="col-md-12">
+									<h2 class="curse-name">Administración del Riesgo y Auditoría Forense</h2>
+								</div>
+								<div class="col-md-5">
+									<figure>
+										<img src="http://placehold.it/610x400" alt="Name of the Curse" class="img-responsive curse-pic">
+										<figcaption>
+											<a class="cta-btn" type="button" data="Administración del Riesgo y Auditoría Forense">MÁS INFO</a>
+										</figcaption>
+									</figure>
+									<div class="description">
+										<p>Adquiere las bases conceptuales y herramientas generales para la administración y prevención de riesgo de delitos económicos y financieros, tales como, así mismo, el uso de herramientas de Auditoría Forense, apoyados en las mejores prácticas.</p>
+										<a class="cta-btn" type="button" data="Administración del Riesgo y Auditoría Forense">MÁS INFO</a>
+									</div>
+								</div>
+								<div class="col-md-7">
+									<div class="bs-example" data-example-id="striped-table">
+										<table class="table table-striped table-bordered"> 
+											<tbody> 
+												<tr> 
+													<th scope="row">Horas</th> 
+													<td>120</td>
+												</tr>
+												<tr>
+													<th scope="row">Inversión</th>
+													<td> $4.990.000 </td>
+												</tr>
+												<tr>
+													<th scope="row">Contenido</th>
+													<td> 
+														<ul>   
+															<li>Módulo 1: Aspectos preliminares y legales de la auditoría forense</li>
+															<li>Módulo 2: Estructura de control y riesgo para la prevención y detección de fraude y corrupción</li>
+															<li>Módulo 3: Criminología</li>
+															<li>Módulo 4: Informática forense </li>
+															<li>Módulo 5: Psicología forense </li>
+															<li>Módulo 6: Técnicas de entrevista</li>
+															<li>Módulo 7: Metodologia de auditoría forense</li>
+															<li>Módulo 8: Estudios de casos: practicas anti fraude</li>
+														</ul>
+													</td>
+												</tr>
+												<tr>
+													<th scope="row">Dirigido a</th>
+													<td>El Diplomado está dirigido principalmente a profesionales que se desempeñen como auditores, revisores fiscales, jefes de seguridad, directores de control, directores de riesgos (tanto para el sector público como el privado). </td>
+												</tr>									
+												<tr>
+													<th scope="row">Fecha de Inicio</th>
+													<td>Agosto 22 de 2017</td>
+												</tr>
+												<tr>
+													<th scope="row">Fecha Finalización</th>
+													<td>Noviembre 28 de 2017</td>
+												</tr>
+												<tr>
+													<th scope="row">Horario</th>
+													<td>Lunes a miércoles de 6:00 p.m. a 9:00 p.m.</td>
+												</tr>
+											</tbody> 
+										</table> 
+									</div>
+								</div>
+							</div>
+						</div>
+						<!-- Termina CURSO Administración del Riesgo y Auditoría Forense-->
+						<!-- CURSO Gestión del Marketing Estratégico-->
+						<div class="col-md-6">
+							<div class="item row">
+								<div class="col-md-12">
+									<h2 class="curse-name">Gestión del Marketing Estratégico</h2>
+								</div>
+								<div class="col-md-5">
+									<figure>
+										<img src="http://placehold.it/610x400" alt="Name of the Curse" class="img-responsive curse-pic">
+										<figcaption>
+											<a class="cta-btn" type="button" data="Gestión del Marketing Estratégico">MÁS INFO</a>
+										</figcaption>
+									</figure>
+									<div class="description">
+										<p>Conoce los elementos más importantes dentro de la Gestión de mercadeo en ambientes organizacionales complejos y globales, junto a las herramientas de la gestión de marketing para desarrollarlas en ambientes empresariales de  alta competencia.</p>
+										<a class="cta-btn" type="button" data="Gestión del Marketing Estratégico">MÁS INFO</a>
+									</div>
+								</div>
+								<div class="col-md-7">
+									<div class="bs-example" data-example-id="striped-table">
+										<table class="table table-striped table-bordered"> 
+											<tbody> 
+												<tr> 
+													<th scope="row">Horas</th> 
+													<td>123</td>
+												</tr>
+												<tr>
+													<th scope="row">Inversión</th>
+													<td> $4.890.000 </td>
+												</tr>
+												<tr>
+													<th scope="row">Contenido</th>
+													<td> 
+														<ul>   
+															<li>Módulo 1: Visión estratégica del Mercadeo.</li>
+															<li>Módulo 2: Business Intelligence.</li>
+															<li>Módulo 3: Branding.</li>
+															<li>Módulo 4: Creatividad e innovación para el desararollo de productos y servicios. </li>
+															<li>Módulo 5: Pricing.</li>
+															<li>Módulo 6: Trade.</li>
+															<li>Módulo 7: Metodologia de auditoría forense</li>
+															<li>Módulo 8: Marketing Digital. </li>
+															<li>Módulo 8: Plan de Mercadeo Estratégico.</li>
+														</ul>
+													</td>
+												</tr>
+												<tr>
+													<th scope="row">Dirigido a</th>
+													<td>Dirigido a profesionales en marketing, área de mercadeo y personas que por su actividad laboral deseen actualizarse Mercadeo Estratégico. </td>
+												</tr>									
+												<tr>
+													<th scope="row">Fecha de Inicio</th>
+													<td>Agosto 28 de 2017</td>
+												</tr>
+												<tr>
+													<th scope="row">Fecha Finalización</th>
+													<td>Diciembre 5 de 2017</td>
+												</tr>
+												<tr>
+													<th scope="row">Horario</th>
+													<td>Lunes a miércoles de 6:00 p.m. a 9:00 p.m.</td>
+												</tr>
+											</tbody> 
+										</table> 
+									</div>
+								</div>
+							</div>
+						</div>
+						<!-- Termina CURSO Gestión del Marketing Estratégico-->
+					</div>
+					<div class="row">
+						<!-- CURSO Gerencia Comercial Estratégica-->
+						<div class="col-md-6">
+							<div class="item row">
+								<div class="col-md-12">
+									<h2 class="curse-name">Gerencia Comercial Estratégica</h2>
+								</div>
+								<div class="col-md-5">
+									<figure>
+										<img src="http://placehold.it/610x400" alt="Name of the Curse" class="img-responsive curse-pic">
+										<figcaption>
+											<a class="cta-btn" type="button" data="Gerencia Comercial Estratégica">MÁS INFO</a>
+										</figcaption>
+									</figure>
+									<div class="description">
+										<p>En mercados cada vez más competitivos, en los cuales los clientes son cada vez más exigentes, se hace necesario que las fuerzas comerciales de sus organizaciones estén preparadas, bajo estas condiciones los argumentos de competitividad transcienden lo técnico-productivo y se ubican en otros campos de acción relacionados con el área de mercadeo y ventas.</p>
+										<a class="cta-btn" type="button" data="Gerencia Comercial Estratégica">MÁS INFO</a>
+									</div>
+								</div>
+								<div class="col-md-7">
+									<div class="bs-example" data-example-id="striped-table">
+										<table class="table table-striped table-bordered"> 
+											<tbody> 
+												<tr> 
+													<th scope="row">Horas</th> 
+													<td>120</td>
+												</tr>
+												<tr>
+													<th scope="row">Inversión</th>
+													<td> $4.190.000 </td>
+												</tr>
+												<tr>
+													<th scope="row">Contenido</th>
+													<td> 
+														<ul>   
+															<li>Módulo 1: Liderazo comercial.</li>
+															<li>Módulo 2: FEl mercadeo como apoyo a la gestión comercial.</li>
+															<li>Módulo 3: Marketing y Canales Digitales. </li>
+															<li>Módulo 4: Modernas tendencias y estrategias de venta. </li>
+															<li>Módulo 5: Planeación de la estrategia de ventas.</li>
+															<li>Módulo 6: Gerencia de Ventas. </li>
+														</ul>
+													</td>
+												</tr>
+												<tr>
+													<th scope="row">Dirigido a</th>
+													<td>Gerentes y directores comerciales, gerentes y directores de ventas, gerentes de mercadeo, gerentes de cuenta, ejecutivos comerciales y personas con experiencia en el área comercial.</td>
+												</tr>									
+												<tr>
+													<th scope="row">Fecha de Inicio</th>
+													<td>Septiembre 5 de 2017</td>
+												</tr>
+												<tr>
+													<th scope="row">Fecha Finalización</th>
+													<td>Diciembre 5 de 2017</td>
+												</tr>
+												<tr>
+													<th scope="row">Horario</th>
+													<td>Martes a jueves de 6:00 a 9:00 p.m.</td>
+												</tr>
+											</tbody> 
+										</table> 
+									</div>
+								</div>
+							</div>
+						</div>
+						<!-- Termina CURSO Gerencia Comercial Estratégica-->
+						<!-- CURSO Habilidades de Liderazgo y Dirección-->
+						<div class="col-md-6">
+							<div class="item row">
+								<div class="col-md-12">
+									<h2 class="curse-name">Habilidades de Liderazgo y Dirección</h2>
+								</div>
+								<div class="col-md-5">
+									<figure>
+										<img src="http://placehold.it/610x400" alt="Name of the Curse" class="img-responsive curse-pic">
+										<figcaption>
+											<a class="cta-btn" type="button" data="Habilidades de Liderazgo y Dirección">MÁS INFO</a>
+										</figcaption>
+									</figure>
+									<div class="description">
+										<p>Fortalezca sus competencias organizacionales y de rol, mejorando sus habilidades de liderazgo y desarrollo de otros.</p>
+										<a class="cta-btn" type="button" data="Habilidades de Liderazgo y Dirección">MÁS INFO</a>
+									</div>
+								</div>
+								<div class="col-md-7">
+									<div class="bs-example" data-example-id="striped-table">
+										<table class="table table-striped table-bordered"> 
+											<tbody> 
+												<tr> 
+													<th scope="row">Horas</th> 
+													<td>120</td>
+												</tr>
+												<tr>
+													<th scope="row">Inversión</th>
+													<td> $4.220.000 </td>
+												</tr>
+												<tr>
+													<th scope="row">Contenido</th>
+													<td> 
+														<ul>   
+															<li>Módulo 1: Ética en los negocios y RSE.</li>
+															<li>Módulo 2: Comunicación efectiva.</li>
+															<li>Módulo 3: Inteligencia emocional. </li>
+															<li>Módulo 4: Resolución de conflictos y negociación. </li>
+															<li>Módulo 5: Liderazgo personal.. </li>
+															<li>Módulo 6: Visión y análisis estratégico.</li>
+															<li>Módulo 7:  Toma de decisiones.</li>
+															<li>Módulo 7: Manejo del tiempo, agenda y reuniones.</li>
+															<li>Módulo 8: Gestión de los procesos.</li>
+															<li>Módulo 9: Gestión de la innovación y la creatividad.</li>
+															<li>Módulo 10:  Liderazgo y trabajo en equipo.</li>
+															<li>Módulo 11: Desarrollo de otros: Coaching y delegación.</li>           
+														</ul>
+													</td>
+												</tr>
+												<tr>
+													<th scope="row">Dirigido a</th>
+													<td>Gerentes de unidades de negocios y personas con potencial para el desempeño de cargos directivos.  </td>
+												</tr>									
+												<tr>
+													<th scope="row">Fecha de Inicio</th>
+													<td>Septiembre 8 de 2017</td>
+												</tr>
+												<tr>
+													<th scope="row">Fecha Finalización</th>
+													<td>Noviembre 25 de 2017</td>
+												</tr>
+												<tr>
+													<th scope="row">Horario</th>
+													<td>Viernes de 5:30 p.m. a 9:30 p.m. y sábados de 7:30 a.m. a 1:30 p.m.</td>
+												</tr>
+											</tbody> 
+										</table> 
+									</div>
+								</div>
+							</div>
+						</div>
+						<!-- Termina CURSO Habilidades de Liderazgo y Dirección-->
+					</div>
 				</div>
 			</div>
-			<div role="tabpanel" class="tab-pane" id="diplomado-virtual">...</div>
+			<div role="tabpanel" class="tab-pane" id="diplomado-virtual">
+				<div class="container-fluid">
+					<div class="row">
+						<!-- CURSO Gerencia de Proyectos-->
+						<div class="col-md-6">
+							<div class="item row">
+								<div class="col-md-12">
+									<h2 class="curse-name">Gerencia de Proyectos</h2>
+								</div>
+								<div class="col-md-5">
+									<figure>
+										<img src="http://placehold.it/610x400" alt="Name of the Curse" class="img-responsive curse-pic">
+										<figcaption>
+											<a class="cta-btn" type="button" data="Gerencia de Proyectos">MÁS INFO</a>
+										</figcaption>
+									</figure>
+									<div class="description">
+										<p>Adquiere un conocimiento integral de las herramientas claves en la formulación, ejecución, control y evaluación de proyectos, de tal forma que la persona esté preparada teórica y técnicamente para enfrentarse a la dirección de proyectos de desarrollo económico, en el ámbito estatal o privado.</p>
+										<a class="cta-btn" type="button" data="Gerencia de Proyectos">MÁS INFO</a>
+									</div>
+								</div>
+								<div class="col-md-7">
+									<div class="bs-example" data-example-id="striped-table">
+										<table class="table table-striped table-bordered"> 
+											<tbody> 
+												<tr> 
+													<th scope="row">Horas</th> 
+													<td>162</td>
+												</tr>
+												<tr>
+													<th scope="row">Inversión</th>
+													<td> $5.700.000 </td>
+												</tr>
+												<tr>
+													<th scope="row">Contenido</th>
+													<td> 
+														<ul>   
+															<li>Módulo 1: Conceptos básicos y alcance de los proyectos.</li>
+															<li>Módulo 2: Funciones de la Gestión Integral de Proyectos.</li>
+															<li>Módulo 3: Principios Básicos del Emprendimiento.</li>
+															<li>Módulo 4: Gestión de Equipos.</li>
+														</ul>
+													</td>
+												</tr>
+												<tr>
+													<th scope="row">Dirigido a</th>
+													<td>Profesionales  de diversas disciplinas que en su labor diaria participen como líderes o Gerentes dentro de un proyecto.</td>
+												</tr>									
+												<tr>
+													<th scope="row">Fecha de Inicio</th>
+													<td>Septiembre 4 de 2017</td>
+												</tr>
+												<tr>
+													<th scope="row">Fecha Finalización</th>
+													<td>Diciembre 11 de 2017</td>
+												</tr>
+												<tr>
+													<th scope="row">Horario</th>
+													<td>Lunes a jueves de 6:00 p.m. a 9:00 p.m.</td>
+												</tr>
+											</tbody> 
+										</table> 
+									</div>
+								</div>
+							</div>
+						</div>
+						<!-- Termina CURSO Gerencia de Proyectos-->
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
 </section>
