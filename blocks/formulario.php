427d30e7610837c3bde199ac6b77d238723a1598
Carlos Rubio Mac BM <carlosblondsk8@gmail.com>
2017-08-01 18:20:28 -0500

    estilos finales

--- blocks/formulario.php
+++ blocks/formulario.php
@@ -15,23 +15,31 @@
 				<div class="form-group">
 					<select name="programa" id="programa" class="form-control" required>
 						<option value="">Programa</option>
-						<option value=""></option>
+						<option value="Actualización Tributaria">Actualización Tributaria</option>
+						<option value="Gestión del Riesgo Operativo y Continuidad del Negocio">Gestión del Riesgo Operativo y Continuidad del Negocio</option>
+						<option value="Estrategias de Marketing y Canales Digitales">Estrategias de Marketing y Canales Digitales</option>
+						<option value="Gestión Inmobiliaria">Gestión Inmobiliaria</option>
+						<option value="Gerencia Financiera">Gerencia Financiera</option>
+						<option value="Finanzas para No Financieros">Finanzas para No Financieros</option>
+						<option value="Administración del Riesgo y Auditoría Forense">Administración del Riesgo y Auditoría Forense</option>
+						<option value="Gestión del Marketing Estratégico">Gestión del Marketing Estratégico</option>
+						<option value="Gerencia Comercial Estratégica">Gerencia Comercial Estratégica</option>
+						<option value="Habilidades de Liderazgo y Dirección">Habilidades de Liderazgo y Dirección</option>
+						<option value="Gerencia de Proyectos">Gerencia de Proyectos</option>
 					</select>
 				</div>
-				<div class="form-group text-center">
+				<div class="form-group-big">
 					<label for="terminos">
 						<input type="checkbox" name="terminos" checked required> 
-						<i>Autorizo a ser contactado por teléfono o correo electrónico.</i>
+						<a href="http://www.javeriana.edu.co/documentos/tratamiento_datos_personales.pdf" target="blank">Autorizo ser contactado por teléfono y correo electrónico, y seguir recibiendo información académica por parte de la Universidad</a>
 					</label>
 				</div>
-				<div class="form-group">
+				<div>
 					<input type="hidden" name="partnerId" value="<?php echo $partnerId ?>">
 					<input type="hidden" name="type" value="<?php echo $type ?>">
 				</div>
-				<div class="form-group">
+				<div class="form-group-big">
 					<button type="submit" class="btn btn-sub center-block">ENVIAR</button>
-				</div>
-				<div class="form-group">
 				</div>
 			</div>	
 		</form>
--- blocks/header.php
+++ blocks/header.php
@@ -8,37 +8,39 @@
 			</div>
 			<div class="modal-body">
 				<form name="contacto" id="contacto2" novalidate>
-					<div class="form-group">
-						<input type="hidden" name="campaignId" value="<?php echo $campaignId ?>">
-						<input type="text" class="form-control" name="nombre" placeholder="Nombre completo" required/>
-					</div>
-					<div class="form-group">
-						<input type="text" class="form-control" name="empresa" placeholder="Empresa" required/>
-					</div>
-					<div class="form-group">
-						<input type="text" class="form-control" name="cargo" placeholder="Cargo" required/>
-					</div>
-					<div class="form-group">
-						<input type="text" class="form-control" name="telefono" placeholder="Teléfono" required/>
-					</div>
-					<div class="form-group">
-						<input type="text" class="form-control" name="correo" placeholder="Email" required/>
-					</div>
-					<div class="form-group">
-						<input type="text" class="form-control" name="programa" id="programa" readonly="readonly"/>
-					</div>
-					<div class="checkbox">
-						<label for="terminos">
-							<input type="checkbox" name="terminos" checked required>
-							<i>Autorizo a ser contactado por teléfono o correo electrónico.</i>
-						</label>
-					</div>
-					<div class="form-group">
-						<input type="hidden" name="partnerId" value="<?php echo $partnerId ?>">
-						<input type="hidden" name="type" value="<?php echo $type ?>">
-					</div>
-					<div class="form-group">
-						<button type="submit" class="btn btn-sub center-block">ENVIAR</button>
+					<div class="form-inputs-groups">
+						<div class="form-group">
+							<input type="hidden" name="campaignId" value="<?php echo $campaignId ?>">
+							<input type="text" class="form-control" name="nombre" placeholder="Nombre completo" required/>
+						</div>
+						<div class="form-group">
+							<input type="text" class="form-control" name="empresa" placeholder="Empresa" required/>
+						</div>
+						<div class="form-group">
+							<input type="text" class="form-control" name="cargo" placeholder="Cargo" required/>
+						</div>
+						<div class="form-group">
+							<input type="text" class="form-control" name="telefono" placeholder="Teléfono" required/>
+						</div>
+						<div class="form-group">
+							<input type="text" class="form-control" name="correo" placeholder="Email" required/>
+						</div>
+						<div class="form-group">
+							<input type="text" class="form-control" name="programa" id="programa" readonly="readonly"/>
+						</div>
+						<div class="form-group-big">
+							<label for="terminos">
+								<input type="checkbox" name="terminos" checked required> 
+								<a href="http://www.javeriana.edu.co/documentos/tratamiento_datos_personales.pdf" target="blank">Autorizo ser contactado por teléfono y correo electrónico, y seguir recibiendo información académica por parte de la Universidad</a>
+							</label>
+						</div>
+						<div>
+							<input type="hidden" name="partnerId" value="<?php echo $partnerId ?>">
+							<input type="hidden" name="type" value="<?php echo $type ?>">
+						</div>
+						<div class="form-group-big">
+							<button type="submit" class="btn btn-sub center-block">ENVIAR</button>
+						</div>
 					</div>
 				</form>
 			</div>
