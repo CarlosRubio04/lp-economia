72c012d07522bbb965630972f3884f819f5c83ef
Carlos Rubio Mac BM <carlosblondsk8@gmail.com>
2017-08-10 16:45:18 -0500

    gracias

---
+++ blocks/gracias.php
@@ -0,0 +1,16 @@
+<section class="hero">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<img src="img/logo.png" alt="Universidad Javeriana" class="img-responsive center-block">
+				<h1 class="text-center">
+					Gracias por escribirnos
+				</h1>
+				<p class="text-center">
+					Pronto estarémos en contacto
+				</p>
+			</div>
+		</div>
+	</div>
+</section>
+<a href="." class="btn btn-sub center-block">VOLVER</a>
